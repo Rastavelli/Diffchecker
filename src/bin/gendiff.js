41da3bfb04f00ec7f8b7332182f5c9472ff48906
Rastavelli <rashid.zhauzhurek@gmail.com>
2018-02-22 22:52:48 +0600

    add action to handle file paths

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import program from 'commander';
+import genDiff from '..';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .option('-f, --format <type>', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((path1, path2) => console.log(genDiff(path1, path2)))
   .parse(process.argv);
