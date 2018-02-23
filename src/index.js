320595fbaf31f198d591b3a547e3f561dc24c6a1
Rastavelli <rashid.zhauzhurek@gmail.com>
2018-02-23 23:57:00 +0600

    refactor remove node mutation

--- src/index.js
+++ src/index.js
@@ -24,13 +24,11 @@ const genDiff = (pathToFile1, pathToFile2) => {
       };
 
       if (has(beforeData, node.name) && has(afterData, node.name)) {
-        node.type = (node.after === node.before) ? 'notChanged' : 'changed';
+        return { ...node, type: (node.after === node.before) ? 'notChanged' : 'changed' };
       } else if (has(beforeData, node.name)) {
-        node.type = 'deleted';
-      } else {
-        node.type = 'added';
+        return { ...node, type: 'deleted' };
       }
-      return node;
+      return { ...node, type: 'added' };
     });
 
   const diffLines = astToRender => astToRender.map(node => lineTypes[node.type](node));
