448d402185814d3e7e14ff44c02c30c8c21dfcf8
Rastavelli <rashid.zhauzhurek@gmail.com>
2018-02-24 01:15:01 +0600

    feat difference between two yaml files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,8 +1,22 @@
 import genDiff from '../src';
 
-test('genDiff', () => {
+test('json difference', () => {
   const filePath1 = '__tests__/__fixtures__/before.json';
   const filePath2 = '__tests__/__fixtures__/after.json';
+  const diffResult = `
+  {
+      host: hexlet.io
+    + timeout: 20
+    - timeout: 50
+    - proxy: 123.234.53.22
+    + verbose: true
+  }`;
+  expect(genDiff(filePath1, filePath2)).toBe(diffResult);
+});
+
+test('yaml difference', () => {
+  const filePath1 = '__tests__/__fixtures__/before.yaml';
+  const filePath2 = '__tests__/__fixtures__/after.yaml';
   const diffResult = `
   {
       host: hexlet.io
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import fs from 'fs';
+import yaml from 'js-yaml';
+import path from 'path';
+
 import { keys, has, union, join } from 'lodash';
 
 const lineTypes =
@@ -10,22 +13,34 @@ const lineTypes =
     added: ({ name, after }) => `+ ${name}: ${after}`,
   };
 
-const genDiff = (pathToFile1, pathToFile2) => {
-  const beforeData = JSON.parse(fs.readFileSync(pathToFile1));
-  const afterData = JSON.parse(fs.readFileSync(pathToFile2));
+const fileExtensionMapping =
+  {
+    '.json': JSON.parse,
+    '.yaml': yaml.safeLoad,
+  };
 
-  const ast = union(keys(beforeData), keys(afterData))
+const fileDataToObj = (pathToFile) => {
+  const extension = path.extname(pathToFile);
+  const content = fs.readFileSync(pathToFile);
+  return fileExtensionMapping[extension](content);
+};
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const beforeObj = fileDataToObj(pathToFile1);
+  const afterObj = fileDataToObj(pathToFile2);
+
+  const ast = union(keys(beforeObj), keys(afterObj))
     .map((key) => {
       const node = {
         name: [key],
         type: '',
-        before: beforeData[key],
-        after: afterData[key],
+        before: beforeObj[key],
+        after: afterObj[key],
       };
 
-      if (has(beforeData, node.name) && has(afterData, node.name)) {
+      if (has(beforeObj, node.name) && has(afterObj, node.name)) {
         return { ...node, type: (node.after === node.before) ? 'notChanged' : 'changed' };
-      } else if (has(beforeData, node.name)) {
+      } else if (has(beforeObj, node.name)) {
         return { ...node, type: 'deleted' };
       }
       return { ...node, type: 'added' };
